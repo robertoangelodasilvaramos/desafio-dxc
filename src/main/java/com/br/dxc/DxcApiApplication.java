d92e57fbdc99a76ee14390cdc6aa94e0cdef3b8f
Roberto <robertoramos@somosadd.com.br>
2025-02-23 20:04:59 -0300

    initial

---
+++ src/main/java/com/br/dxc/DxcApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.dxc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DxcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DxcApiApplication.class, args);
+	}
+
+}
