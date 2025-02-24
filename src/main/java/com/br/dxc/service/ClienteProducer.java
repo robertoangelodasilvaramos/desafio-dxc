98ce58bcf34a830605bebe16c4566fe7ede27542
Roberto <robertoramos@somosadd.com.br>
2025-02-24 14:14:12 -0300

    correções

--- src/main/java/com/br/dxc/service/ClienteProducer.java
+++ src/main/java/com/br/dxc/service/ClienteProducer.java
@@ -1,20 +1,17 @@
 package com.br.dxc.service;
 
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class ClienteProducer {
 
-
-    private final RabbitTemplate rabbitTemplate;
-
-    public ClienteProducer(final RabbitTemplate rabbitTemplate) {
-        this.rabbitTemplate = rabbitTemplate;
-    }
-
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
     public void enviarMensagem(String mensagem) {
         rabbitTemplate.convertAndSend("clienteQueue", mensagem);
